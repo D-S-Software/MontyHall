71ecb8e5ca3c094bd3c11c48d1323abc16dcc9cc
Stra2020 <lukestrachan55@gmail.com>
2020-04-16 19:22:56 -0400

    This is a kinda test

--- src/ProcessGame.java
+++ src/ProcessGame.java
@@ -3,6 +3,7 @@ public class ProcessGame {
     /**Arrays of each selected door (true/false for car/goat) for each iteration*/
     private boolean[] stayResults, switchResults;
     private int iterations;
+    private int plaseDelete;
 
     /**Creates a game and makes a random selection. Then stores the results in respective array for each iteration
      *
